9fe60b9640e2e2d90bf94436b5d744ef12187edb
Someleze Lucky mazwi <slmazwi@gmail.com>
2026-05-08 08:56:14 +0200

    Create app.config.ts

---
+++ app.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "@tanstack/react-start/config";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  vite: {
+    plugins: [
+      tsconfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ],
+  },
+  server: {
+    preset: "vercel",
+  },
+});
