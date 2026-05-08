d6b0b7c1f0b61b1908d3278672de450ea3cb4bed
Someleze Lucky mazwi <slmazwi@gmail.com>
2026-05-08 09:06:51 +0200

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,11 @@
-import { defineConfig } from "@tanstack/react-start/config";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  vite: {
-    plugins: [
-      tsconfigPaths({
-        projects: ["./tsconfig.json"],
-      }),
-    ],
-  },
-  server: {
-    preset: "vercel",
-  },
+  tanstackStart: {
+    server: { 
+      // Pointing to your new location
+      entry: "api/index.ts",
+      preset: "vercel" 
+    },
+  }
 });
