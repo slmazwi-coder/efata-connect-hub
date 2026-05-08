e8dc0d804431e8a74c4f4d9edee1d1fb6a0153dc
Someleze Lucky mazwi <slmazwi@gmail.com>
2026-05-08 09:07:35 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
+    tanstackStart(),
     tsconfigPaths()
   ],
-  // We ensure Vite doesn't look for index.html as the primary entry point
-  // during the TanStack Start build phase.
   build: {
+    // Explicitly setting output for Vercel
+    outDir: ".output/public",
     emptyOutDir: true,
   }
 });
